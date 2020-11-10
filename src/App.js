08a521504fcf775fd5de31c1f4acfe79945a43e5
matheusAuraconsultoria <matheus@auraconsultoria.com.br>
2020-11-10 18:21:56 -0300

    Adicionada lógica de criação e exclusão dos todos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import Header from './components/layout/Header';
 import Todos from './components/Todos';
-
+import AddTodo from './components/AddTodo';
 import './App.css';
 
 class App extends Component {
@@ -14,7 +15,7 @@ class App extends Component {
       {
         id: 2,
         title: 'Take out the trash pt2',
-        completed: false
+        completed: true
       },
       {
         id: 3,
@@ -23,11 +24,29 @@ class App extends Component {
       },
     ]
   }
+  
+  // Toggle Complete
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }) });
+  }
+
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)]})
+  }
 
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos}/>
+        <div className="container">
+          <Header />
+          <AddTodo />
+          <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+        </div>        
       </div>
     );
   }
